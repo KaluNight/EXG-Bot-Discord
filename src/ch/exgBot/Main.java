8f238af0e17ffe04a1b6b4e6593e19979e04daf7
KaluNight <lucadasilva003@gmail.com>
2018-09-25 22:22:57 +0200

    Add login system

--- src/ch/exgBot/Main.java
+++ src/ch/exgBot/Main.java
@@ -1,9 +1,23 @@
 package ch.exgBot;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
 public class Main {
 
+	private static JDA jda;
+	
 	public static void main(String[] args) {
-
+		try {
+			jda = new JDABuilder(AccountType.BOT).setToken(args[0]).buildBlocking();
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("You must provide a token.");
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
 	}
 
 }
